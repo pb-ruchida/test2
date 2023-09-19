73a504ffa9b88208ec952119cd6519824d3d3a09
Ren Uchida <uchida.ren@pi-pe.cop>
2023-09-19 14:46:05 +0900

    2nd

--- xmlrpc-sample.py
+++ xmlrpc-sample.py
@@ -3,16 +3,18 @@ from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 
 args = sys.argv
+ip = ''
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/sample',)
     def __init__(self, request, client_address, server):
-        print(client_address[0])
+        global ip
+        ip = client_address[0]
         print(args[1])
         SimpleXMLRPCRequestHandler.__init__(self, request, client_address, server)
 
 def sample(data):
-    res = {'StatusCode': '200', 'XxxxxXxxxxxxXxxx': 'xxx########'}
+    res = {'StatusCode': ip, 'XxxxxXxxxxxxXxxx': 'xxx########'}
     return res
 
 with SimpleXMLRPCServer(('0.0.0.0', 9090), requestHandler=RequestHandler) as server:
